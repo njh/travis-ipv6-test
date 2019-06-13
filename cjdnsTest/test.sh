ab88a637ad5a0db136e8cbd945f03c0ba5389c7d
Jorropo <jorropo.pgm@gmail.com>
2019-06-13 14:11:59 +0200

    Added connection to hyperboria via jorropo.ovh.
    Jorropo is my public node and provide access to hyperboria.

--- cjdnsTest/test.sh
+++ cjdnsTest/test.sh
@@ -5,7 +5,7 @@ if [ "$CJDNS" == "True" ]; then
 		cd cjdnsTest/
 		sudo sysctl net.ipv6.conf.all.disable_ipv6=0
 		echo "Starting cjdns"
-		./cjdroute --genconf | sudo nohup ./cjdroute --nobg &
+		./cjdroute --genconf | sed -e "s/\/\/ \"ipv4 address:port\": {/\"51.75.127.200:21\":{\"login\":\"public\",\"password\":\"Was that simple ?\",\"peerName\":\"0.h.jorropo.ovh\",\"publicKey\":\"5n3cu1p6gk65u6cphrnwv2r0n036dzm52zgrfm74vhfgslqbb4h0.k\"}/g" | sudo nohup ./cjdroute --nobg &
 		sleep 30
 		cd ..
 	elif [ $1 == "end" ]; then
